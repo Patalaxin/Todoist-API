3bf86509b3df049570991d30cc9c5169e08d4cd6
spatalaxin <spatalahin@flo.team>
2024-02-29 15:27:51 +0300

    testing commit

--- playwright/tests/InstallationTest.spec.ts
+++ playwright/tests/InstallationTest.spec.ts
@@ -2,7 +2,7 @@ const { test } = require('../PageObject/Utilities/MainPageFixture')
 
 // import { MainPage } from '../PageObject/Pages/MainPage';
 
-test('Test12', async ({ mainPage, page }) => {
+test('Test', async ({ mainPage, page }) => {
   await mainPage.goto()
   await mainPage.gotoInstallation()
 });
